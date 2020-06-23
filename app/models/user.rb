ea6e740241f51c4d487d9937d932bbeff2b3a827
Gordy <gordybullen@gmail.com>
2020-06-23 02:01:56 -0700

    Complete SPIRE methods for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,29 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+  def self.find_by_credentials(login_credential, password)
+    user = User.find_by(username: login_credential)
+    !user ? user = User.find_by(email: login_credential)
+    return nil unless user
+    user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password=password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+  
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
 end
