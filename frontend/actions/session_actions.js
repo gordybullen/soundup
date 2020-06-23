8dbf13fcc60a5ad4cbd1d0003c405c3557e3190c
Gordy <gordybullen@gmail.com>
2020-06-23 14:49:47 -0700

    Create users and session reducers and store

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,7 +2,7 @@ import * as SessionApiUtil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
-export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
 const receiveCurrentUser = currentUser => {
   return {
@@ -19,7 +19,7 @@ const logoutCurrentUser = () => {
 
 const receiveErrors = errors => {
   return {
-    type: RECEIVE_ERRORS,
+    type: RECEIVE_SESSION_ERRORS,
     errors
   };
 };
@@ -27,13 +27,13 @@ const receiveErrors = errors => {
 export const login = user => dispatch => {
   return SessionApiUtil.login(user)
     .then(loggedInUser => dispatch(receiveCurrentUser(loggedInUser)))
-    .fail(errors => dispatch(receiveErrors(errors)));
+    .fail(res => dispatch(receiveErrors(res.responseJSON)));
 };
 
 export const logout = () => dispatch => {
   return SessionApiUtil.logout()
     .then(() => dispatch(logoutCurrentUser()))
-    .fail(errors => dispatch(receiveErrors(errors)));
+    .fail(res => dispatch(receiveErrors(res.responseJSON)));
 };
 
 export const signup = user => dispatch => {
--- frontend/soundup.jsx
+++ frontend/soundup.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { signup, login, logout } from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
+
   ReactDOM.render(<h1>Welcome to SoundUp</h1>, root);
 
   // for testing only!!!
-
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   window.signup = signup;
   window.login = login;
   window.logout = logout;
