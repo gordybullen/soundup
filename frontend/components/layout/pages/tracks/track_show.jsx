b817a15c517973716686e63133639211f122ade6
Gordy <gordybullen@gmail.com>
2020-07-28 22:39:33 -0700

    Style track show and add audio player and edit buttons

--- frontend/components/layout/pages/tracks/track_show.jsx
+++ frontend/components/layout/pages/tracks/track_show.jsx
@@ -5,10 +5,12 @@ import { requestTrack, deleteTrack } from '../../../../actions/track_actions';
 const mSTP = (state, ownProps) => {
   const track = state.entities.tracks[ownProps.match.params.trackId];
   const artist = track ? state.entities.users[track.userId] : null;
+  const currentUser = state.session.id;
   // debugger
   return {
     track: track,
-    artist: artist
+    artist: artist,
+    currentUser: currentUser,
   };
 }
 
@@ -20,25 +22,46 @@ const mDTP = dispatch => {
 }
 
 const TrackShow = props => {
-  const { track, artist } = props;
+  const { track, artist, currentUser } = props;
 
   useEffect(() => {
     props.requestTrack(props.match.params.trackId);
   }, []);
 
+  const trackEdit = () => {
+    return (
+      track && currentUser === track.userId ? (
+        <div className="track-edit-container">
+          <button className="track-edit-btn edit">
+            Edit
+          </button>
+          <button className="track-edit-btn delete">
+            Delete
+          </button>
+        </div>
+      ) : null
+    );
+  }
+
   return (
-    <div className="track-show-container">
-      <div className="track-show-left">
-        <div className="track-info track-artist">
-          {artist ? artist.username : null}
-        </div><br/>
-        <div className="track-info track-title">
-          {track ? track.title : null}
+    <div className="track-show">
+      <div className="track-show-container">
+        <div className="track-show-left">
+          <div className="track-info-container">
+            <div className="track-info track-artist">
+              {artist ? artist.username : null}
+            </div><br/>
+            <div className="track-info track-title">
+              {track ? track.title : null}
+            </div>
+          </div>
+          <audio controls src={track ? track.audioFileUrl : null}></audio>
+        </div>
+        <div className="track-show-right">
+          <img src={track ? track.imageUrl : null} className="track-image" />
         </div>
       </div>
-      <div className="track-show-right">
-        <img src={track ? track.imageUrl : null} className="track-image" />
-      </div>
+      {trackEdit()}
     </div>
   );
 }
